7e8d7a593f7312c66429bc92ed65ff8a7f69eb46
cykimbb <cykimbb@gmail.com>
2026-04-10 13:40:33 +0900

    feat: 마우스 우클릭 및 이미지 저장 방지

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import { useEffect } from "react";
 import { BrowserRouter } from "react-router-dom";
 import Router from "./Router";
 import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
+  useEffect(() => {
+    document.oncontextmenu = function () {
+      return false;
+    };
+  }, []);
+
   return (
     <BrowserRouter>
       <ScrollToTop />
